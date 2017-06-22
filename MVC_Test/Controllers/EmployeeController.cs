8108dc19902149eacb166a413ea5a11c8408c1d8
Gaurav Lodha <gauravlodha77@gmail.com>
2017-06-22 13:49:44 +0530

    Initial Commit

---
+++ MVC_Test/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Test.Models;
+
+namespace MVC_Test.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Details()
+        {
+            Employee e1 = new Employee()
+            {
+                Id = 1,
+                Name = "Roopali",
+                Gender = "Female",
+                City = "Mumbai"
+            };
+            return View(e1);
+        }
+    }
+}
---
+++ MVC_Test/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Test.Models
+{
+    public class Employee
+    {
+        public int Id { set; get; }
+        public String Name { set; get; }
+        public String Gender { set; get; }
+        public String City { set; get; }
+    }
+}
